d375aaf65dab8c86ca70b32c6d6c21b1310ae31a
Luiz Fernando Lanna André de Assis <luiz.flaassis@hsl.org.br>
2021-07-15 14:09:57 -0300

    start

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      monsters:[],
+      search:''
+    }
+
+    //this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+  }
+
+  handleChange = (e) =>{
+    this.setState({search:e.target.value })
+  }
+
+  render(){
+    const {monsters,search} = this.state;
+    const filteredMonsters = monsters.filter(monster=> monster.name.toLowerCase().includes(search.toLowerCase()));
+    return (
+    <div className="App">
+      <SearchBox placeholder="search monster" handleChange={ this.handleChange} />
+        <CardList monsters={filteredMonsters}></CardList>
+    </div>
+  );
+  }
+}
+
+export default App;
